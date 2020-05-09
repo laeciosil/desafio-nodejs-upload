d7cd05c318b38db165a7a516431896f39c84d5f8
laecio12 <laecio12silva@gmai.com>
2020-05-09 20:15:30 -0300

    Conclusão do defasio upload co node.js

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -9,9 +9,30 @@ interface Balance {
 }
 
 @EntityRepository(Transaction)
-class TransactionsRepository extends Repository<Transaction> {
+export class TransactionsRepository extends Repository<Transaction> {
   public async getBalance(): Promise<Balance> {
-    // TODO
+    const incomes = await this.find({ where: { type: 'income' } });
+
+    const outcomes = await this.find({ where: { type: 'outcome' } });
+
+    const totalIncomes = incomes.reduce((total: number, item: Transaction) => {
+      return total + item.value;
+    }, 0);
+
+    const totalOutcomes = outcomes.reduce(
+      (total: number, item: Transaction) => {
+        return total + item.value;
+      },
+      0,
+    );
+
+    const balance = {
+      income: totalIncomes,
+      outcome: totalOutcomes,
+      total: totalIncomes - totalOutcomes,
+    };
+
+    return balance;
   }
 }
 
